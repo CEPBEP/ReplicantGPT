c67359b4675e35985e4fc7c73cbe77f1644eceec
Jesse Andrews <anotherjesse@gmail.com>
2023-04-16 10:31:54 -0400

    a little cleanup

--- agent/runner.js
+++ agent/runner.js
@@ -1,41 +1,40 @@
 import { Configuration, OpenAIApi } from "openai";
 import * as files from "./files_system.js";
-import {performCommit, commitInstructions} from './commit_system.js';
+import { performCommit, commitInstructions } from './commit_system.js';
+import { promises as fs } from 'fs';
 
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+const chat = async ({ model, messages }) => {
+    const logFn = "logs/" + (new Date().toISOString().replace(/:/g, '-') + Math.random().toString(36).substring(7) + '.json');
+    await fs.writeFile(logFn, JSON.stringify({ messages, model }, null, 2));
+    const response = await openai.createChatCompletion({ model, messages })
+    const message = response.data.choices[0].message
+    await fs.writeFile(logFn, JSON.stringify({ messages, model, response: message }, null, 2));
+    return message;
+}
 
 const run = async ({ model, prompt, projectDir }) => {
+    const messages = [
+        { role: "system", content: files.system },
+        { role: "user", content: prompt }
+    ]
 
-    const completion = await openai.createChatCompletion({
-        model,
-        messages: [{
-            role: "system", content: files.system,
-        }, {
-            role: "user", content: prompt,
-        }],
-    });
+    let message = await chat({ model, messages });
+    messages.push(message)
 
-    const changeTxt = completion.data.choices[0].message.content;
-    const changes = files.performOperations(changeTxt, projectDir);
-    console.log(changes);
+    console.log({ message })
+    const changes = files.performOperations(message.content, projectDir);
+    console.log({ changes });
 
-    const results = await openai.createChatCompletion({
-        model: model,
-        messages: [
-            { role: "system", content: files.system, },
-            { role: "user", content: prompt, },
-            { role: 'assistant', content: changeTxt },
-            { role: 'user', content: commitInstructions }
-        ],
-    });
+    messages.push({ role: 'user', content: commitInstructions });
 
-    const commitMessage = results.data.choices[0].message.content;
+    message = await chat({ model, messages });
 
-    await performCommit({ message: commitMessage, changes }, projectDir);
+    await performCommit({ message: message.content, changes }, projectDir);
 }
 
 export default run;
