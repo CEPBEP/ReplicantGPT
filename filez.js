01f9e41805b55204d3cea3f2df7293478a426360
Charlie Holtz <charlieholtz@gmail.com>
2023-04-16 08:18:53 -0700

    recurse through files

--- filez.js
+++ filez.js
@@ -35,29 +35,6 @@ export async function listFiles(currentPath = project_dir) {
   }
 }
 
-// export async function listFiles() {
-//     try {
-//         const items = await fs.readdir(project_dir);
-//         const fileDetails = [];
-//         for (const item of items) {
-//             const itemPath = path.join(project_dir, item);
-//             const stats = await fs.stat(itemPath);
-//             if (stats.isFile()) {
-//                 fileDetails.push({
-//                     path: itemPath,
-//                     ctime: stats.ctime.toISOString(),
-//                     mtime: stats.mtime.toISOString(),
-//                     size: stats.size
-//                 });
-//             }
-//         }
-//         return fileDetails;
-//     } catch (err) {
-//         console.error(err);
-//         throw err;
-//     }
-// }
-
 export async function getFile(filename) {
   filename = path.join(project_dir, filename);
   return await fs.readFile(filename, "utf8");
